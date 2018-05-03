872f4b6e84bfd43b68f5cec89fcf79d4914468c3
Edda Pétursdóttir <edda@trappa.is>
2018-05-03 13:44:27 +0000

    Adding gitignore and moving git

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+gulp.task('serve', function() {
+  // place code for your default task here
+  console.log('hello edda');
+});
